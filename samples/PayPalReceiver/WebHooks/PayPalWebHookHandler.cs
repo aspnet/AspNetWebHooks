cf1beb734453f541d4ec55e5d23eeab4e77ca7b5
Marusyk <romamarusyk@gmail.com>
2018-01-21 00:10:02 +0200

    Add PayPal sample

---
+++ samples/PayPalReceiver/WebHooks/PayPalWebHookHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace PayPalReceiver.WebHooks
+{
+    public class GitHubWebHookHandler : WebHookHandler
+    {
+        public GitHubWebHookHandler()
+        {
+            this.Receiver = PaypalWebHookReceiver.ReceiverName;
+        }
+
+        public override Task ExecuteAsync(string generator, WebHookHandlerContext context)
+        {
+            // For more information about PayPal WebHook payloads, please see 
+            // 'https://developer.paypal.com/docs/integration/direct/webhooks/'
+            JObject entry = context.GetDataOrDefault<JObject>();
+
+            return Task.FromResult(true);
+        }
+    }
+}
