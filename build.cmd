cf019052f3bc0c5fa12188b2b57d93f2bf2ba524
Pranav K <prkrishn@hotmail.com>
2019-12-04 15:20:36 -0800

    Update build scripts to be compatible with the build environment

--- build.cmd
+++ build.cmd
@@ -7,12 +7,33 @@ mkdir bin
 
 :Build
 
-REM Find the most recent 32bit MSBuild.exe on the system. Require v12.0 (installed with VS2013) or later since .NET 4.0
-REM is not supported. Always quote the %MSBuild% value when setting the variable and never quote %MSBuild% references.
-set MSBuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
-if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
-if not exist %MSBuild% (
-  echo Could not find msbuild.exe. Please run this from a Visual Studio developer prompt
+REM Find the most recent 32bit MSBuild.exe on the system. Require v15.0 (installed with VS2017) or later.
+REM Use `vswhere` for the search since %ProgramFiles(x86)%\msbuild\15.0\Bin\MSBuild.exe almost never exists.
+set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist %vswhere% (
+  set VsWhere="%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
+)
+if not exist %vswhere% (
+  REM vswhere.exe not in normal locations; check the Path.
+  for %%X in (vswhere.exe) do (
+    set vswhere="%%~$PATH:X"
+  )
+)
+if not exist %vswhere% (
+  echo Could not find vswhere.exe. Please run this from a Visual Studio developer prompt.
+  goto BuildFail
+)
+
+set InstallDir=
+for /f "usebackq tokens=*" %%i in (`%vswhere% -latest -prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set InstallDir=%%i
+)
+if exist "%InstallDir%\MSBuild\Current\Bin\MSBuild.exe" (
+   set MSBuild="%InstallDir%\MSBuild\Current\Bin\MSBuild.exe"
+) else if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+  set MSBuild="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+) else (
+  echo Could not find MSBuild.exe. Please install the VS2017 BuildTools component or a workload that includes it.
   goto BuildFail
 )
 
@@ -23,11 +44,13 @@ if not exist %StorageEmulator% (
   goto BuildFail
 )
 
+%StorageEmulator% stop 2> NUL
+%StorageEmulator% clear 2> NUL
 %StorageEmulator% start 2> NUL
 
 if "%1" == "" goto BuildDefaults
 
-%MSBuild% WebHooks.msbuild /m /nr:false /t:%* /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+%MSBuild% WebHooks.msbuild /m /nr:false /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal /t:%
 if %ERRORLEVEL% neq 0 goto BuildFail
 goto BuildSuccess
 
